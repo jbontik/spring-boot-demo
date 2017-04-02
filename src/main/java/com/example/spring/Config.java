70e57107ed8d08b6487851b723f48e023cbcb26b
Jean Bontikous <jbontikous@yahoo.fr>
2017-04-02 10:45:57 +0200

    First commit

---
+++ src/main/java/com/example/spring/Config.java
@@ -0,0 +1,10 @@
+package com.example.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jbont_000 on 4/2/2017.
+ */
+@Configuration
+public class Config {
+}
